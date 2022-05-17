7cd67105de30d08ae777441f04afb60f13e57862
PranitDethe <103254744+PranitDethe@users.noreply.github.com>
2022-05-17 22:02:00 +0530

    Add files via upload

---
+++ TASK 2/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './appcontainer/App';
+import 'tachyons';
+
+
+ReactDOM.render(
+    <App/>
+    ,
+  document.getElementById('root')
+);
+
+
